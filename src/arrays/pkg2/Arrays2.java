a06a235ed5f708236821cc2a65f9ccd46dad4bd5
Dilraj Singh <Diluppal2399@gmail.com>
2019-05-14 14:45:35 -0400

    first  commit

---
+++ src/arrays/pkg2/Arrays2.java
@@ -0,0 +1,28 @@
+package arrays.pkg2;
+
+/**
+ *
+ * @author Rich Smith at ZenOfProgramming.com
+ */
+public class Arrays2
+{
+   /**
+    * @param args the command line arguments
+    */
+   public static void main (String[] args)
+   {
+      /* char[] arr = {'a', 'b', 'c', 'd'};
+       *
+       * for (int i = 0; i < arr.length; i++) {
+       * System.out.print(arr[i]);
+       *
+       * }
+       * System.out.print("\n");
+       * for (int i = arr.length - 1; i >= 0; i--) {
+       *
+       * System.out.println(arr[i]); */
+
+
+   }
+
+}
